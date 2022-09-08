9f126ba981f4a44cae307616489e3bdce20ecb81
Suvish Thoovamalayil <suvish.thoovamalayil@contentstack.com>
2022-09-08 17:14:11 +0530

    Add capability to customize all configuration values via environment variables

--- README.md
+++ README.md
@@ -4,8 +4,22 @@ This is a simple web app for demoing Contentstack's OAuth capabilities
 
 To run locally,
 
+Create a new `.env` file in the root directory. You can customize the following environment variables here.
+
 ```
-$ CLIENT_SECRET=<client-secret-here> npm run dev
+APP_URL="https://app.contentstack.com"
+API_URL="https://api.contentstack.io"
+APP_UID="628ba758d922620018019fac"
+CLIENT_ID="QhEyHzxDIYeGZFN_"
+CLIENT_SECRET="<needs to be provided>"
+API_KEY="blt167ab95ac670dcac"
+CONTENT_TYPE_UID="test_content_type"
+ENTRY_UID="blt3a7fea4498429bc2"
+OAUTH_REDIRECT_URI="http://localhost:3000/oauth/callback"
 ```
 
-All configuration values are kept in `app/constants.ts`.
+Then, start the application using the fillowing command.
+
+```
+$ npm run dev
+```
--- app/constants.ts
+++ app/constants.ts
@@ -1,10 +1,15 @@
-const ENTRY_UID = 'blt3a7fea4498429bc2';
-const CONTENT_TYPE_UID = 'test_content_type';
-export const CLIENT_ID = 'QhEyHzxDIYeGZFN_';
+const APP_URL = process.env['APP_URL'] || 'https://app.contentstack.com';
+const API_URL = process.env['API_URL'] || 'https://api.contentstack.io';
+const ENTRY_UID = process.env['ENTRY_UID'] || 'blt3a7fea4498429bc2';
+const CONTENT_TYPE_UID = process.env['CONTENT_TYPE_UID'] || 'test_content_type';
+export const CLIENT_ID = process.env['CLIENT_ID'] || 'QhEyHzxDIYeGZFN_';
 export const CLIENT_SECRET = process.env['CLIENT_SECRET'];
-const APP_UID = '628ba758d922620018019fac';
-export const OAUTH_REDIRECT_URI = 'http://localhost:3000/oauth/callback';
-export const ENTRY_URL = `https://api.contentstack.io/v3/content_types/${CONTENT_TYPE_UID}/entries/${ENTRY_UID}`;
-export const REDIRECT_URL = `https://app.contentstack.com/#!/apps/${APP_UID}/authorize?client_id=${CLIENT_ID}&redirect_uri=${OAUTH_REDIRECT_URI}&response_type=code`;
-export const TOKEN_URL = 'https://app.contentstack.com/apps-api/apps/token';
-export const API_KEY = 'blt167ab95ac670dcac';
+if (!CLIENT_SECRET) {
+  throw new Error('CLIENT_SECRET environment variable is not defined!');
+}
+const APP_UID = process.env['APP_UID'] || '628ba758d922620018019fac';
+export const OAUTH_REDIRECT_URI = process.env['OAUTH_REDIRECT_URI'] || 'http://localhost:3000/oauth/callback';
+export const ENTRY_URL = `${API_URL}/v3/content_types/${CONTENT_TYPE_UID}/entries/${ENTRY_UID}`;
+export const REDIRECT_URL = `${APP_URL}/#!/apps/${APP_UID}/authorize?client_id=${CLIENT_ID}&redirect_uri=${OAUTH_REDIRECT_URI}&response_type=code`;
+export const TOKEN_URL = `${APP_URL}/apps-api/apps/token`;
+export const API_KEY = process.env['API_KEY'] || 'blt167ab95ac670dcac';
